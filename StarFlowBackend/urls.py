d2198db917bbadebeddbb4126053930089b7700c
zhoubin1022 <2031348494@qq.com>
2021-11-10 23:29:37 +0800

    项目细节修改

--- StarFlowBackend/urls.py
+++ StarFlowBackend/urls.py
@@ -1,4 +1,4 @@
-"""starFlow URL Configuration
+"""StarFlowBackend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- StarFlowBackend/wsgi.py
+++ StarFlowBackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for starFlow project.
+WSGI config for StarFlowBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starFlow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StarFlowBackend.settings')
 
 application = get_wsgi_application()
