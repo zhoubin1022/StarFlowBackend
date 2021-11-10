810bfa2536b37a6410f157459b2cdf87df37c982
HuDunYu <1410196583@qq.com>
2021-11-11 00:36:46 +0800

    User&Join_request 表创建

--- User/models.py
+++ User/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+'''
+openid 是 用户唯一标识符
+session_key 是 密钥
+username 是 Github用户名
+password 是 GitHub密码
+'''
+
 
 class User(models.Model):
     openid = models.CharField(max_length=100)
     session_key = models.CharField(max_length=100)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+
+
+'''
+repo 是 Repository表id
+user 是 User表id,申请加入的成员
+identity 是 身份/职位
+request_time 是 申请时间
+'''
 
 
 class Join_request(models.Model):
