851e0d08fd3ffeea83bcd108807c01656c6d7b16
ColemanHuang <1195083225@qq.com>
2021-11-11 21:42:22 +0800

    add 添加任务功能

--- Task/models.py
+++ Task/models.py
@@ -7,16 +7,18 @@ class Task(models.Model):
     repo = models.ForeignKey('Repository.Repository', on_delete=models.CASCADE)  # Repository表 id
     member = models.ForeignKey('Repository.Member', on_delete=models.CASCADE)  # Member表 id
     task_info = models.CharField(max_length=100)  # 任务信息
-    status = models.IntegerField()  # 任务状态
+    status = models.IntegerField(default=0)  # 任务状态
     deadline = models.DateTimeField()  # 截止日期
     record = models.ForeignKey('Record', on_delete=models.CASCADE)  # Task_Record表 id
 
 
 class Record(models.Model):
     submit_time = models.DateTimeField(auto_now_add=True)  # 提交时间
+    task_id = models.ForeignKey('Task', on_delete=models.CASCADE)  # 任务 id
+    submitMember = models.ForeignKey('Repository.Member', related_name="submit", on_delete=models.CASCADE)  # 提交项目人员 id
     submit_info = models.CharField(max_length=100)  # 提交描述
     request_id = models.IntegerField()  # 在仓库 pull request 的 id
-    checkMember = models.ForeignKey('Repository.Member', on_delete=models.CASCADE)  # Member表 id
+    checkMember = models.ForeignKey('Repository.Member', related_name="check", on_delete=models.CASCADE)  # Member表 id
     check_time = models.DateTimeField(auto_now=True)  # 审核时间
     result = models.IntegerField()  # 审核结果
     comment = models.CharField(max_length=100)  # 评价信息
--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from Task.models import Task, Record
+from Repository.models import Repository, Member
+from User.models import User, Join_request
 # Create your views here.
+# def getSubmissionHistory(request):  # 获取任务的历史提交记录
+#     ret =
+
+def addTask(request):
+    if request.method == 'POST':
+        task_info = str(request.POST.get('task_info'))
+        deadline = str(request.POST.get('deadline'))
+        rename = str(request.POST.get('repo_name'))
+        username = str(request.POST.get('username'))
+    repo = Repository.objects.filter(repo_name=rename)
+    user = User.objects.filter(user_name=username)
+    member = User.objects.filter(repo[0], user[0])
+    task = Task.objects.create(repo=repo.id, member=member.id, task_info=task_info, deadline=deadline)
+
+    task.save()
