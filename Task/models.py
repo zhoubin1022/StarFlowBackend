c5714bd8526838a9124ddd3485d3d1d094599ef2
zhoubin1022 <2031348494@qq.com>
2021-11-11 21:33:23 +0800

    add 微信登录和小程序登录

--- Task/models.py
+++ Task/models.py
@@ -7,16 +7,20 @@ class Task(models.Model):
     repo = models.ForeignKey('Repository.Repository', on_delete=models.CASCADE)  # Repository表 id
     member = models.ForeignKey('Repository.Member', on_delete=models.CASCADE)  # Member表 id
     task_info = models.CharField(max_length=100)  # 任务信息
-    status = models.IntegerField()  # 任务状态
+    status = models.IntegerField(default=0)  # 任务状态
     deadline = models.DateTimeField()  # 截止日期
-    record = models.ForeignKey('Record', on_delete=models.CASCADE)  # Task_Record表 id
+    record = models.ForeignKey('Record', on_delete=models.CASCADE, null=True)  # Task_Record表 id
 
 
 class Record(models.Model):
     submit_time = models.DateTimeField(auto_now_add=True)  # 提交时间
     submit_info = models.CharField(max_length=100)  # 提交描述
+    submitMember = models.ForeignKey('Repository.Member', to_field='id',
+                                     on_delete=models.CASCADE, related_name='submit', null=True)
     request_id = models.IntegerField()  # 在仓库 pull request 的 id
-    checkMember = models.ForeignKey('Repository.Member', on_delete=models.CASCADE)  # Member表 id
+    checkMember = models.ForeignKey('Repository.Member', to_field='id',
+                                    on_delete=models.CASCADE, related_name='checking', null=True)
+    # Member表 id
     check_time = models.DateTimeField(auto_now=True)  # 审核时间
-    result = models.IntegerField()  # 审核结果
-    comment = models.CharField(max_length=100)  # 评价信息
+    result = models.IntegerField(null=True)  # 审核结果
+    comment = models.CharField(max_length=100, null=True)  # 评价信息
--- User/views.py
+++ User/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import requests
+from django.core import serializers
+from django.http import JsonResponse
+from User.models import User
 
-# Create your views here.
+
+# 微信登录
+def wxLogin(request):
+    if request.method == 'POST':
+        result = {"message": 'success', "data": []}
+        code = request.POST.get('code')
+        user_info = getUserInfo(code)
+        openid = user_info['openid']
+        session_key = user_info['session_key']
+        new_user = User(openid=openid, session_key=session_key)
+        new_user.save()
+        user = User.objects.filter(openid=openid)
+        result['data'] = serializers.serialize('python', user)
+        return JsonResponse(result)
+
+
+# 根据code获取相应信息
+def getUserInfo(code):
+    params = {
+        "appid": '',
+        "secret": '',
+        "js_code": code,
+        "grant_type": 'authorization_code'
+    }
+    result = requests.get('https://api.weixin.qq.com/sns/jscode2session', params=params, timeout=3, verify=False)
+    return result.json()
+
+
+# github登录
+def githubLogin(request):
+    if request.method == 'POST':
+        result = {"message": 'success', "data": []}
+        openid = request.POST.get('openid')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        users = User.objects.filter(openid=openid)
+        if not users:
+            return JsonResponse({"message": '小程序登录状态出错'})
+        user = users.first()
+        if not (user.openid == openid):
+            return JsonResponse({"message": '小程序登录状态错误'})
+        user.username = username
+        user.password = password
+        user.save()
+        return JsonResponse(result)
