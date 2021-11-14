e7107d3f0fef0d3eb387e9249a65036c10542316
tiankuanyu <1175611675@qq.com>
2021-11-14 22:25:45 +0800

    add test代码

--- Repository/urls.py
+++ Repository/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from Repository import admin
-
+from Repository import views
 urlpatterns = [
-
+    path('database_query', views.database_query),
+    path('database_query_task_list', views.database_query_task_list),
+    path('database_project_insert', views.database_project_insert),
+    path('identity_change', views.identity_change),
+    path('aa', views.aa)
 ]
