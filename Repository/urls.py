05399f91cf51c5bcc32ed128ebfa843331fad14a
tiankuanyu <1175611675@qq.com>
2021-11-15 22:10:55 +0800

    add test

--- Repository/urls.py
+++ Repository/urls.py
@@ -4,7 +4,6 @@ from Repository import views
 urlpatterns = [
     path('database_query', views.database_query),
     path('database_query_task_list', views.database_query_task_list),
-    path('database_project_insert', views.database_project_insert),
-    path('identity_change', views.identity_change),
-    path('aa', views.aa)
+    path('database_project_insert_one', views.database_project_insert_one),
+    path('identity_change', views.identity_change)
 ]
--- Repository/views.py
+++ Repository/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+import os
+import re
 from django.http import JsonResponse,HttpResponse
 from Repository.models import Repository,Member
 from Task.models import Task
@@ -9,48 +10,45 @@ from django.core import serializers
 import os
 import django
 from StarFlowBackend import settings
-
-
 def database_query(request):  # 展示该用户参与的项目列表
+    data = []
     if request.method == 'POST':
-        userid = int(request.POST.get('uid'))  # 获取用户id
-        repository_id = Member.objects.filter(user_id=userid)  # 找出改用户的所有仓库
-        for i in repository_id:  # 对仓库循环 找出仓库表中的id与repository_id相等的仓库的所有值
-            records = Repository.objects.filter(id=i.repo_id)
-            result = {"message": 'success', "data": serializers.serialize('python', records)}
-            return JsonResponse(result)
-    return JsonResponse({"message": 'wrong'})
+        username = request.POST.get('username')  # 获取用户名
+        repository_id = Member.objects.filter(username=username)  # 找出该用户的所有仓库
+        if repository_id.exists():
+            for i in repository_id:
+                records = Repository.objects.get(id=i.repo_id)
+                data.append(records)
+            return JsonResponse(data)
+        else:
+            return JsonResponse({"message1": 'the user is not exist'})
+
+    return JsonResponse({"message2": 'wrong'})
 
 def database_query_task_list(request):  # 展示项目的任务列表
     if request.method == 'POST':
         repo_id = int(request.POST.get('repo_id'))  # 获取仓库id
-        records1 = Repository.objects.filter(id=repo_id)  # 获取改仓库里的所有任务信息包括，完成数，待审核数等等，看前端需要什么
-        records2 = Task.objects.filter(repo_id=repo_id)   # 查询Task表找出所有任务信息，包括任务状态，截至日期等等
-        result = {"message": 'success', "data1": serializers.serialize('python', records1), "data2": serializers.serialize('python', records2)}
+        records = Task.objects.filter(repo_id=repo_id).values('task_info','status','deadline')  # 查询Task表找出所有任务信息，包括任务状态，截至日期等等
+        return HttpResponse(records)
+    return JsonResponse({"message": 'wrong'})
+
+def database_project_insert_one(request):  # 用户选择一个项目，把该项目放入数据库，并将当前用户设为超级管理员
+    if request.method == 'POST':
+        url = str(request.POST.get('url'))
+        repo_name = str(request.POST.get('repo_name'))
+        user_id=str(request.POST.get('user_id'))
+        new_record_repo = Repository.objects.create(url=str(url) , repo_name=str(repo_name))
+        new_record_repo.save()  # 记录保存
+        HttpResponse(new_record_repo.id)
+        records = Repository.objects.filter(url=url,repo_name=repo_name)
+        result = {"message": 'success', "data": serializers.serialize('python',records)}
         return JsonResponse(result)
     return JsonResponse({"message": 'wrong'})
 
 
 
-def database_project_insert(request):  # 用户选择一个项目，把该项目放入数据库，并将当前用户设为超级管理员
-    if request.method == 'POST':
-        repo_member = request.POST.get('repo_member')  # 传入项目信息和用户的信息
-        url = request.POST.get('url')
-        repo_name = request.POST.get('repo_name')
-        finished = request.POST.get('finished')
-        checking = request.POST.get('checking')
-        incomplete = request.POST.get('incomplete')
-        username = request.POST.get('username')
-        repo_id = request.POST.get('repo_id')
-        user_id = request.POST.get('user_id')
-        new_record_repo = Repository.objects.create(repo_member=repo_member, url=url, repo_name=repo_name, finished=finished, checking=checking, incomplete=incomplete)
-        # 产生新仓库记录
-        new_record_repo.save()  # 记录保存
-        new_record_user = Member.objects.create(repo_id=new_record_repo.id, username=username, identity=1, user_id=user_id)
-        # 产生新用户记录 identity身份信息为整数，1代表超级管理员，0代表项目普通成员，-1代表游客
-        new_record_user.save()  # 记录保存
-        return JsonResponse({"message": 'setting success',"data":new_record_repo.id})
-    return JsonResponse({"message": 'wrong'})
+
+
 
 
 def identity_change(request):  # 项目人员身份调整
@@ -58,28 +56,29 @@ def identity_change(request):  # 项目人员身份调整
         repo_id = request.POST.get('repo_id')
         user_id = request.POST.get('user_id')
         operation = request.POST.get('operation')
-        repo = Repository.objects.filter(repo_id=repo_id)
-        if repo.exist():   # 判断该仓库是否存在
-            user=repo.objects.get(user_id__exact=user_id)
-            if user.exist():  # 判断该仓库中是否存在这个用户
-                if operation == 1:  # 操作码为1，表示要把这个成员身份设置成管理员
-                    if user.identity == 0: # 成员原来身份是普通成员
-                        user.identity = 1  # 设置为1，变为超级管理员
-                        user.save()
-                    elif user.identity == 1:  # 成员本来就是超级管理员
-                        return JsonResponse("the member is a super manager already")
+        user = Member.objects.get(user_id=user_id, repo_id=repo_id)
+        if operation == '1':  # 操作码为1表示要将一个成员设置成管理员
+            if user.identity == 0:
+                user.identity = 1
+                user.save()
+                return JsonResponse({"message":'success set a super manager'})
+            elif user.identity == 1:
+                return JsonResponse({"message":'the member is a super manager already'})
+            elif user.identity == 2:  # 将2设置为游客身份，不能操作
+                return JsonResponse({"message": '权限不足'})
+        if operation == '0':  # 操作码为0表示要将管理员身份还原为普通成员
+            if user.identity == 1:
+                user.identity = int(0)
+                user.save()
+                return JsonResponse({"message": 'success set a common member'})
+            elif user.identity == int(0):
+                return JsonResponse({"message": 'the member is a common member already'})
+            elif user.identity == 2:  # 将2设置为游客身份，不能操作
+                return JsonResponse({"message": '权限不足'})
+# 操作码错误直接报错，没有找到这个项目get函数会报错
+        return JsonResponse({"message": 'wrong'})
 
-                elif operation == 0:  # 操作码为0，表示要把这个成员身份从超级管理员设置成普通成员
-                    if user.identity == 1:  # 原来是超级管理员
-                        user.identity = 0
-                        user.save()
-                    elif user.identity == 0:  # 原来是普通成员
-                        return JsonResponse("the member is a common member already")
-        return JsonResponse("wrong")
 
-
-def aa(request):
-    return HttpResponse("123123123")
 
 
 
