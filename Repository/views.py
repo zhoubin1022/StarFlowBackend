00dce932d9c0ba9b3191c932df4c4d0cc8967795
tiankuanyu <1175611675@qq.com>
2021-11-17 11:24:03 +0800

    test

--- Repository/views.py
+++ Repository/views.py
@@ -25,22 +25,34 @@ def database_query(request):  # 展示该用户参与的项目列表
 
     return JsonResponse({"message2": 'wrong'})
 
+dic1={'tpye':'incomplte'}
+dic2={'type':'checking'}
+dic3={'type':'finish'}
 def database_query_task_list(request):  # 展示项目的任务列表
     if request.method == 'POST':
         repo_id = int(request.POST.get('repo_id'))  # 获取仓库id
-        records = Task.objects.filter(repo_id=repo_id).values('task_info','status','deadline')  # 查询Task表找出所有任务信息，包括任务状态，截至日期等等
-        return HttpResponse(records)
+        # 查询Task表找出所有任务信息，包括任务状态，截至日期等等
+        records1 = Task.objects.filter(repo_id=repo_id, status=0) .values('task_info','status','deadline','task_name')
+        records2 = Task.objects.filter(repo_id=repo_id, status=1).values('task_info', 'status', 'deadline','task_name')
+        records3 = Task.objects.filter(repo_id=repo_id, status=2).values('task_info', 'status', 'deadline','task_name')
+        record = records1 | records2
+        record = record | records3
+        result=record.order_by("status")
+        return HttpResponse(result)
     return JsonResponse({"message": 'wrong'})
 
 def database_project_insert_one(request):  # 用户选择一个项目，把该项目放入数据库，并将当前用户设为超级管理员
     if request.method == 'POST':
         url = str(request.POST.get('url'))
         repo_name = str(request.POST.get('repo_name'))
-        user_id=str(request.POST.get('user_id'))
-        new_record_repo = Repository.objects.create(url=str(url) , repo_name=str(repo_name))
+        user_id = str(request.POST.get('user_id'))
+        username = str(request.POST.get('username'))
+        new_record_repo = Repository.objects.create(url=str(url), repo_name=str(repo_name))
         new_record_repo.save()  # 记录保存
-        HttpResponse(new_record_repo.id)
-        records = Repository.objects.filter(url=url,repo_name=repo_name)
+        #return HttpResponse(new_record_repo.id)
+        new_record_member = Member.objects.create(repo_id_id=new_record_repo.id, user_id_id=user_id, username=username,identity=1)
+        new_record_member.save()
+        records = Repository.objects.filter(id=new_record_repo.id)
         result = {"message": 'success', "data": serializers.serialize('python',records)}
         return JsonResponse(result)
     return JsonResponse({"message": 'wrong'})
