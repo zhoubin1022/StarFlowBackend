3527b65dcf22e900eb53b5d75f9cac85855783bb
HuDunYu <1410196583@qq.com>
2021-11-26 17:35:13 +0800

    feature:测试加入项目请求信息列表的接口

--- User/tests.py
+++ User/tests.py
@@ -1,5 +1,25 @@
 from django.test import TestCase
+import os
+import requests
+import json
+
+host = "http://127.0.0.1:8000/user"
+headers = {'Accept': '*/*'}
 
 # Create your tests here.
-def test_wxlogin(self):
-    response = self.client.post('')
+
+request_info_data = {"user": 1, "repo": 1}
+
+
+def test(method, url, body_data=None):
+    url = host + url
+    response_data = requests.post(url, data=body_data, headers=headers)
+    print(response_data.status_code)
+    response_data = json.loads(response_data.content.decode("utf-8"))
+    if response_data['message'] == "success":
+        print(url + " 成功!")
+    else:
+        print(url + " 失败!" + response_data['message'])
+
+
+test("POST", "/request_info", request_info_data)
